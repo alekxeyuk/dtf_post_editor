71291cf60fa103f847f3dcaa70b76a4a554487bc
LeLuCh <alekx@alumni.nottingham.ac.uk>
2020-06-09 20:26:07 +1000

    bruh login dialog

--- gui.py
+++ gui.py
@@ -1,11 +1,14 @@
 import wx
 import requests
 import mimetypes
+import json
 from requests_toolbelt.multipart.encoder import MultipartEncoderMonitor
 from pathlib import Path
 from threading import Thread
 from pubsub import pub
 from post_editor import Post
+import sys
+import os
 
 def convert_bytes(num):
     """
@@ -15,6 +18,80 @@ def convert_bytes(num):
         if num < 1024.0:
             return "%3.1f %s" % (num, x)
         num /= 1024.0
+
+
+class LoginDialog(wx.Dialog):
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        self.SetSize(300, 240)
+        self.SetIcon(wx.Icon(getResourcePath("favicon.ico")))
+
+        vb = wx.BoxSizer(wx.VERTICAL)
+        self.emailOrToken = wx.RadioBox(self, id=wx.ID_ANY, label="Login Type", choices=['Email', 'Token'], style=wx.RA_SPECIFY_COLS)
+        self.emailOrToken.Bind(wx.EVT_RADIOBOX, self.changeLoginType)
+        vb.Add(self.emailOrToken, flag=wx.EXPAND | wx.ALL, border=10)
+
+        hb = wx.BoxSizer(wx.HORIZONTAL)
+        self.emailText = wx.TextCtrl(self)
+        self.emailText.SetHint('Email...')
+        self.passwordText = wx.TextCtrl(self)
+        self.passwordText.SetHint('Password...')
+        hb.Add(self.emailText, 1, flag=wx.EXPAND | wx.ALL, border=10)
+        hb.Add(self.passwordText, 1, flag=wx.EXPAND | wx.ALL, border=10)
+
+        vb.Add(hb, flag=wx.EXPAND | wx.ALL, border=10)
+
+        hb2 = wx.BoxSizer(wx.HORIZONTAL)
+        bOk = wx.Button(self, wx.ID_ANY, label='Login', size=wx.DefaultSize)
+        bOk.Bind(wx.EVT_BUTTON, self.LoginClick)
+        bCn = wx.Button(self, wx.ID_CANCEL, label='Cancel', size=wx.DefaultSize)
+        hb2.Add(bOk, 1, flag=wx.EXPAND | wx.ALL, border=10)
+        hb2.Add(bCn, 1, flag=wx.EXPAND | wx.ALL, border=10)
+
+        vb.Add(hb2, flag=wx.EXPAND | wx.ALL, border=10)
+
+        self.SetSizer(vb)
+        self.Centre(wx.BOTH)
+
+    def LoginClick(self, event):
+        if self.emailOrToken.GetSelection() and len(self.emailText.Value) > 60:
+            r = requests.get("https://dtf.ru/auth/check?mode=raw", data={"mode": "raw"}, cookies={'osnova-remember': self.emailText.Value}).json()
+            if r['rc'] == 200:
+                with open('settings.json', 'w') as f:
+                    f.write(json.dumps({'osnova-remember': self.emailText.Value}))
+                self.Parent.post = Post(cookies_file='settings.json')
+                self.EndModal(wx.ID_OK)
+            else:
+                wx.MessageBox('Неправильные данные.', 'Info', wx.OK_DEFAULT, parent=self)
+                self.emailText.Clear()
+        elif not self.emailOrToken.GetSelection() and self.emailText.Value and self.passwordText.Value:
+            r = requests.post("https://dtf.ru/auth/simple/login", data={"values[login]": self.emailText.Value, "values[password]": self.passwordText.Value, "mode": "raw"}, headers={"x-this-is-csrf": "THIS IS SPARTA!"})
+            r_json = r.json()
+            print(r_json)
+            if r_json.get('rc', 400) == 200:
+                with open('settings.json', 'w') as f:
+                    f.write(json.dumps({'osnova-remember': r.cookies.get_dict().get('osnova-remember')}))
+                self.Parent.post = Post(cookies_file='settings.json')
+                self.EndModal(wx.ID_OK)
+            else:
+                wx.MessageBox('Неправильные данные.', 'Info', wx.OK_DEFAULT, parent=self)
+                self.emailText.Clear()
+                self.passwordText.Clear()
+        else:
+            wx.MessageBox('Неправильные данные.', 'Info', wx.OK_DEFAULT, parent=self)
+
+    def changeLoginType(self, event):
+        if self.emailOrToken.GetSelection():
+            self.emailText.SetHint('Token...')
+            self.emailText.Clear()
+            self.passwordText.Hide()
+            self.Layout()
+        else:
+            self.emailText.SetHint('Email...')
+            self.emailText.Clear()
+            self.passwordText.Clear()
+            self.passwordText.Show()
+            self.Layout()
 
 class DownloadThread(Thread):
     """Downloading thread"""
@@ -55,10 +132,21 @@ class DownloadThread(Thread):
 
 #---------------------------------------------------------------------------
 
+def getResourcePath(filename):
+    try:
+        basePath = sys._MEIPASS
+    except Exception:
+        basePath = ''
+    path = os.path.join(basePath, filename)
+    print(path)
+    return path
+
+#---------------------------------------------------------------------------
+
 class MyFrame(wx.Frame):
     def __init__(self, parent, title):
         super().__init__(parent, title=title, pos = wx.DefaultPosition, size = wx.Size(290, 140), style = wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER ^ wx.MAXIMIZE_BOX|wx.TAB_TRAVERSAL)
-        self.SetIcon(wx.Icon("favicon.ico"))
+        self.SetIcon(wx.Icon(getResourcePath("favicon.ico")))
         self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
         self.panel = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
@@ -96,6 +184,34 @@ class MyFrame(wx.Frame):
         self.uploaded = 0
         self.fileList = list()
 
+        self.post = None
+
+        self.TryToLogin()
+
+        while not self.post:
+            with LoginDialog(self, title='Login', style= wx.CAPTION | wx.CLOSE_BOX) as dlg:
+                res = dlg.ShowModal()
+                if res == wx.ID_CANCEL:
+                    self.Close()
+                    self.Destroy()
+                    break
+                elif res == wx.ID_OK:
+                    self.Show()
+
+    def TryToLogin(self):
+        if Path('settings.json').is_file():
+            with open('settings.json', 'r') as f:
+                try:
+                    env_j = json.load(f)
+                    self.post = Post(cookies_file='settings.json')
+                    if self.post.check_auth():
+                        self.Show()
+                    else:
+                        wx.MessageBox('Неправильные данные.', 'Info', wx.OK_DEFAULT, parent=self)
+                        self.post = None
+                except json.decoder.JSONDecodeError:
+                    wx.MessageBox('Файл поврежден.', 'Info', wx.OK_DEFAULT, parent=self)
+                    self.post = None
 
     def RecursiveChange(self, event):
         self.recursive = self.recursiveCheck.IsChecked()
@@ -152,12 +268,10 @@ class MyFrame(wx.Frame):
         DownloadThread(self.uploadSize, self.fileList)
 
     def uploadComplete(self, img_list):
-        test_post = Post('test', 74342)
-        r = test_post.session.get("https://dtf.ru/auth/check?mode=raw", data={"mode": "raw"}).json()
-        test_post.subsite_id = r['data']['id']
-        test_post.add_media_list(img_list, cover_count=2)
-        test_post.add_text_block('#wxPythonProstagma', anchor='wxPythonProstagma', cover=False)
-        test_post.save_draft()
+        self.post.subsite_id = self.post.user_id
+        self.post.add_media_list(img_list, cover_count=2)
+        self.post.add_text_block('#wxPythonProstagma', anchor='wxPythonProstagma', cover=False)
+        self.post.save_draft()
 
    
     def updateProgress(self, count):
@@ -180,6 +294,6 @@ class MyFrame(wx.Frame):
 
 if __name__ == '__main__':
     app = wx.App()
-    frame = MyFrame(None, 'DTF uploader v1.0')
-    frame.Show()
+    frame = MyFrame(None, title='DTF uploader v1.0')
+    # frame.Show()
     app.MainLoop()
